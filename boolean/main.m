f5f3b57548b04f807f93846f1cb242e89c013452
dhabhai-srlife <hdhabhai@srlife.net>
2018-03-14 11:33:31 -0400

    Initial Commit

---
+++ boolean/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  boolean
+//
+//  Created by ITMAC1 on 9/21/17.
+//  Copyright © 2017 Senior Life Insurance Company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    
+    // has valid US drivers license
+    bool hasLicense;
+    hasLicense = TRUE;
+    NSLog(@"Has License: %i", hasLicense);
+    
+    return 0;
+}
